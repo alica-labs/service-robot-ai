a553889ab32c526cbb0b123b6643e2e33d146b4f
Stephan Opfer <opfer@vs.uni-kassel.de>
2019-09-18 18:23:56 +0200

    - format

--- srg_worldmodel/src/wm/SRGSimData.cpp
+++ srg_worldmodel/src/wm/SRGSimData.cpp
@@ -1,10 +1,10 @@
 #include "srg/wm/SRGSimData.h"
 
 #include "srg/SRGWorldModel.h"
-#include "srgsim/World.h"
 #include "srgsim/Object.h"
-#include "srgsim/ServiceRobot.h"
 #include "srgsim/SRGEnums.h"
+#include "srgsim/ServiceRobot.h"
+#include "srgsim/World.h"
 
 namespace srg
 {
@@ -18,22 +18,23 @@ SRGSimData::SRGSimData(SRGWorldModel* wm)
 
     // ATTENTION: This path/file is not the same as the simulator
     // is starting, please make sure that both files have the same content!
-    this->world = new srgsim::World((*sc).getConfigPath()+"/textures/Department.tmx");
+    this->world = new srgsim::World((*sc).getConfigPath() + "/textures/Department.tmx");
 }
 
 SRGSimData::~SRGSimData() {}
 
-void SRGSimData::processPerception(srgsim::SimPerceptions simPerceptions) {
-    for(srgsim::Perception perception : simPerceptions.perceptions) {
-        switch(perception.type) {
-            case srgsim::Type::Robot: {
-                srgsim::Object *robot = this->world->addObject(perception.objectID, perception.type);
-                if (this->world->placeObject(robot, srgsim::Coordinate(perception.x, perception.y))) {
-                    this->world->addRobot(static_cast<srgsim::ServiceRobot*>(robot));
-                }
-            }   break;
-            default:
-                std::cerr << "SRGSimData::processPerception(): Unknown perception received!" << std::endl;
+void SRGSimData::processPerception(srgsim::SimPerceptions simPerceptions)
+{
+    for (srgsim::Perception perception : simPerceptions.perceptions) {
+        switch (perception.type) {
+        case srgsim::Type::Robot: {
+            srgsim::Object* robot = this->world->addObject(perception.objectID, perception.type);
+            if (this->world->placeObject(robot, srgsim::Coordinate(perception.x, perception.y))) {
+                this->world->addRobot(static_cast<srgsim::ServiceRobot*>(robot));
+            }
+        } break;
+        default:
+            std::cerr << "SRGSimData::processPerception(): Unknown perception received!" << std::endl;
         }
     }
 }
