1dd9b1f7c545d4086307d8a13c2912a06f9cb460
Stephan Opfer <opfer@vs.uni-kassel.de>
2019-09-18 17:47:32 +0200

    - copied textures also into etc - started to implement world also in worldmodel

--- srg_worldmodel/src/wm/SRGSimData.cpp
+++ srg_worldmodel/src/wm/SRGSimData.cpp
@@ -1,20 +1,8 @@
 #include "srg/wm/SRGSimData.h"
 
 #include "srg/SRGWorldModel.h"
-
-#include <SystemConfig.h>
-#include <engine/AlicaClock.h>
-
-#include <exception>
-#include <iostream>
-#include <map>
-#include <memory>
-#include <utility>
-
-using std::make_shared;
-using std::shared_ptr;
-using supplementary::InfoBuffer;
-using supplementary::InformationElement;
+#include "srgsim/World.h"
+#include "srgsim/SRGEnums.h"
 
 namespace srg
 {
@@ -26,11 +14,25 @@ SRGSimData::SRGSimData(SRGWorldModel* wm)
     this->wm = wm;
     this->sc = this->wm->getSystemConfig();
 
-    // data buffers
-
+    // ATTENTION: This path/file is not the same as the simulator
+    // is starting, please make sure that both files have the same content!
+    this->world = new srgsim::World((*sc).getConfigPath()+"/textures/Department.tmx");
 }
 
 SRGSimData::~SRGSimData() {}
+
+void SRGSimData::processPerception(srgsim::SimPerceptions simPerceptions) {
+
+    for(srgsim::Perception perception : simPerceptions.perceptions) {
+        switch(perception.type) {
+            case srgsim::Type::Robot:
+                // TODO
+                break;
+            default:
+                std::cerr << "SRGSimData::processPerception(): Unknown perception received!" << std:endl;
+        }
+    }
+}
 
 } // namespace wm
 } /* namespace srg */
